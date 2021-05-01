13e832036ce4cec1ae09f7bb86d3663f4ee6ab77
Oséias da Silva Martins <50338986+ozmartins@users.noreply.github.com>
2021-05-01 18:52:41 -0300

    added entity validations

---
+++ src/Hard.Business/Services/SupplierService.cs
@@ -0,0 +1,42 @@
+﻿using Hard.Business.Interfaces;
+using Hard.Business.Models;
+using Hard.Business.Models.Validations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hard.Business.Services
+{
+    public class SupplierService : BaseService, ISupplierService
+    {
+        public Task Create(Supplier supplier)
+        {
+            if (!ExecuteValidation(new SupplierValidator(), supplier) || !ExecuteValidation(new AddressValidator(), supplier.Address))
+                return Task.CompletedTask;
+            else
+                return Task.CompletedTask;
+        }
+
+        public Task Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Update(Supplier supplier)
+        {
+            if (!ExecuteValidation(new SupplierValidator(), supplier) || !ExecuteValidation(new AddressValidator(), supplier.Address))
+                return Task.CompletedTask;
+            else
+                return Task.CompletedTask;
+        }
+
+        public Task UpdateAddress(Address address)
+        {
+            if (!ExecuteValidation(new AddressValidator(), address))
+                return Task.CompletedTask;
+            else
+                return Task.CompletedTask;
+        }
+    }
+}
